a832fc21d6a36c91cb3e9c425f13e28a079a5106
Tae Yi Im <mintchodan@naver.com>
2022-09-20 14:22:11 +0900

    Initial commit

---
+++ sec02_primarydatatype/src/primarydatatype_2/primarydatatype_2.java
@@ -0,0 +1,32 @@
+package primarydatatype_2;
+
+public class primarydatatype_2 {
+	public static void main(String[] args) {
+		
+		
+		char Value1 = 'A';
+        char Value2 = '°¡';
+        char Value3 = '3';
+        System.out.println(Value1);
+        System.out.println(Value2);
+        System.out.println(Value3);
+        System.out.println();
+        
+        
+        char Value4 = 65;
+        char Value5 = 0xac00;
+        char Value6 = 51;
+        System.out.println(Value4);
+        System.out.println(Value5);
+        System.out.println(Value6);
+        System.out.println();
+        
+               
+        char Value7 = '/u0041';
+        char Value8 = '/uac00';
+        char Value9 = '/u0033';
+        System.out.println(Value7);
+        System.out.println(Value8);
+        System.out.println(Value9);
+		}
+	}
